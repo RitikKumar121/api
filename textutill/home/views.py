515dda7f28f25f129e5454acd2ae3308f121a66f
RITIK KUMAR <73395641+RitikKumar121@users.noreply.github.com>
2023-01-19 17:29:36 +0530

    Update views.py

--- textutill/home/views.py
+++ textutill/home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 
-# Create your views here.
+
 
 
 def home(request):
@@ -12,30 +12,27 @@ def home(request):
 def removepunc(request):
     if request.method == 'GET':
         djtext = request.GET.get('text')
-        #analyze = djtext.upper()
-    #    print(djtext)
-        # params={'removetext':analyze}
-        # https://api.github.com/repos/RitikKumar121/blog/languages
+     
         respons1 = requests.get(
-            'https://api.github.com/users/'+djtext).json()  # user name ko link ke sath jorne ke liye ayese kiye hai
+            'https://api.github.com/users/'+djtext).json() 
         respons = requests.get(
             'https://api.github.com/users/'+djtext+'/repos').json()
-        # respons is list type which is conatin dictionary as a element
+      
 
         if len(respons) > 2:
-            resultList = list(respons[0] .values())  # conversion dict to list
+            resultList = list(respons[0] .values())  
             print(resultList[3])
-        # print(type(resultList))
+      
             full_name = resultList[3]
             language = requests.get(
             'https://api.github.com/repos/'+full_name+'/languages').json()
-        # for multiple context pass karne ke liye
+     
             context = {'respons': respons,
                    'respons1': respons1, 'language': language}
             return render(request, "removepunc.html", context)
-        # return render(request,"removepunc.html",params)
+      
         else:
             return HttpResponse("User Not Found ")
     else:
-        #    
+          
         return HttpResponse("Something Wrong")
